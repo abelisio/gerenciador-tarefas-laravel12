e9dae76fd0bcaa6fdd3be080505e69c8392acaa5
abelisio <adriano.belisio@gmail.com>
2025-03-19 02:17:47 -0300

    atualizacao das rotas

--- routes/web.php
+++ routes/web.php
@@ -9,17 +9,10 @@ use App\Models\Responsavel;
 Route::get('/', [HomeController::class, 'index'])->name('index');
 Route::get('/tarefas/listar', [tarefaController::class, 'index'])->name('tarefas.index');
 Route::get('/tarefas/create', [tarefaController::class, 'create'])->name(name: 'tarefas.create');
-//Route::POST('/tarefas/create', [tarefaController::class, 'store'])->name(name: 'tarefas.store');
 Route::post('/store-tarefa', [tarefaController::class, 'store'])->name('tarefas.store');
-
 Route::put('/tarefas/{tarefa}', [TarefaController::class, 'atualizarStatus'])->name('tarefas.atualizarStatus');
-
 Route::get('/tarefas/{tarefa}', [tarefaController::class, 'show'])->name('tarefas.show');
-
 Route::get('/tarefas/responsavel', [ResponsavelController::class, 'create'])->name(name: 'tarefas.create');
-
-
 Route::get('/tarefas/{id}/edit', [TarefaController::class, 'edit'])->name('tarefas.edit');
 Route::put('/tarefas/{id}', [TarefaController::class, 'update'])->name('tarefas.update');
-
 Route::delete('/tarefas/{id}', [TarefaController::class, 'destroy'])->name('tarefas.destroy');
